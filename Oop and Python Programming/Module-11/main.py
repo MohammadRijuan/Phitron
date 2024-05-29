777ac32db43fafba850dd3b739127c27cd359c77
Md Rijuan <mohammadrijuan2529@gmail.com>
2024-05-29 07:21:38 +0600

    commit

--- Oop and Python Programming/Module-11/main.py
+++ Oop and Python Programming/Module-11/main.py
@@ -4,7 +4,8 @@ from persons import*
 def main():
 
     school=School('NMC','Chandgaon')
-    
+
+    # Add classroom
     eight=Classroom('eight')
     school.add_classroom(eight)
 
@@ -13,6 +14,33 @@ def main():
     
     ten=Classroom('ten')
     school.add_classroom(ten)
+
+    #Add students 
+    Rijuan=Student('Rijuan',eight)
+    school.student_admission(Rijuan)
+    
+    Monju=Student('Monju',eight)
+    school.student_admission(Monju)
+    
+    Sifat=Student('Sifat',nine)
+    school.student_admission(Sifat)
+
+    # subjects
+    physics_teacher=Teacher('Nurul islam')
+    physics=subject('Physics',physics_teacher)
+    eight.add_subject(physics)
+
+    chemistry_teacher=Teacher('Ayub sir')
+    chemistry=subject('chemistry',chemistry_teacher)
+    eight.add_subject(chemistry)
+    
+    math_teacher=Teacher('kamal sir')
+    math=subject('math',math_teacher)
+    eight.add_subject(math)
+
+
+    eight.take_semester_exam()
+
 
     print(school)
 
