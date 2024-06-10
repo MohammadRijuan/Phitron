2bb8010db79600aacca48fa9daac9b220ee928f5
Md Rijuan <mohammadrijuan2529@gmail.com>
2024-06-10 13:58:48 +0600

    commit

---
+++ Database/Assignment-1/NO-3.sql
@@ -0,0 +1,22 @@
+DATA --->
+
+1. Data is all about an individual record or unit....
+2.Its an unorganised fact
+3.Data refers to raw information that consists of basic facts and figures
+4.As an example , Rijuan has got 10marks in individual task...Here Rijuan and 10 marks both are data
+
+
+Information --->
+
+1. Information is gathering a group of data in a table
+2. It follows the organised way
+3. Information depends on the data
+4. Information makes  more convenient of ones record
+5. Example---
+ 
+ ------Student--------
+ID     NAME     ADDRESS
+01     Rijuan   CTG
+02     Sifat    Dhaka
+
+Here we can get the information of every student of an institute....
---
+++ Database/Assignment-1/NO-5.sql
@@ -0,0 +1,26 @@
+USE assignment;
+
+CREATE table  EMPLOYEE
+(
+    EMPLOYEE_ID INT PRIMARY KEY,
+    FIRST_NAME VARCHAR(50) UNIQUE,
+    LAST_NAME VARCHAR(50) NOT NULL,
+    AGE INT ,
+    DEPARTMENT VARCHAR(50) NOT NULL
+);
+
+INSERT INTO EMPLOYEE
+(EMPLOYEE_ID,FIRST_NAME,LAST_NAME,AGE,DEPARTMENT)  VALUES (1,'JOHN','DOE',28,'SALES'),
+														  (2,'JANE','SMITH',32,'MARKETING'),
+                                                          (3,'MICHAEL','JHONSON',35,'FINANCE'),
+                                                          (4,'SARAH','BROWN',30,'HR'),
+                                                          (5,'WILLIAM','DAVIS',25,'ENGINEERING'),
+                                                          (6,'EMILY','WILSON',28,'SALES'),
+                                                          (7,'ROBERT','LEE',33,'MARKETING'),
+                                                          (8,'LAURA','HALL',29,'FINANCE'),
+                                                          (9,'THOMAS','WHITE',31,'HR'),
+                                                          (10,'OLIVIA','CLARK',27,'ENGINEERING');
+                                                          
+														
+                                                        
+SELECT distinct DEPARTMENT FROM EMPLOYEE
