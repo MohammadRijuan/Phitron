a13100155dbb188c194c166e32ee2c97f13859b8
Md Rijuan <mohammadrijuan2529@gmail.com>
2024-06-29 23:02:52 +0600

    commit

---
+++ Database/Module-10/ATTRIBUTES.sql
@@ -0,0 +1,11 @@
+/* ATTRIBUTES
+THERE ARE 3TYPES OF ATTRIBUTES
+1.COMPOSITE               2.MULTIVALUED             3.DERIEVED
+
+  STUDENT                 STUDENT                    STUDENT
+ROLL                     ROLL                      ROLL 
+NAME- FIRST NAME         NAME                      NAME
+      LAST NAME          PHONE(MORE THAN ONE)      DATE OF BIRTH
+												   AGE
+ADDRESS- PERMENANT                                 **WE CAN ASSURE AGE BY SEEING DATE OF BIRTH 
+		 PRESENT
