f1b15301452ec969b621287049380c5b1c237086
Md Rijuan <mohammadrijuan2529@gmail.com>
2024-07-04 00:14:30 +0600

    commit

---
+++ Database/Module-11.5/Entity_attributes.sql
@@ -0,0 +1,19 @@
+/*  Entity attributes can vary depending on the context, but here are some common types:
+
+Basic Attribute: This type describes the most fundamental characteristics of an entity. For example, for a "Person" entity, basic attributes could include name, age, gender, and address.
+
+Derived Attribute: Derived attributes are those that can be derived or calculated from other attributes. For instance, if you have a "Date of Birth" attribute, you can derive the "Age" attribute from it.
+
+Composite Attribute: Composite attributes are made up of multiple simple attributes. For example, an "Address" attribute can be composed of "Street," "City," "State," and "Zip Code" attributes.
+
+Single-valued Attribute: This type of attribute holds only a single value for each instance of the entity. For example, a "Phone Number" attribute for a person.
+
+Multi-valued Attribute: Multi-valued attributes can hold multiple values for each instance of the entity. For instance, a "Languages Spoken" attribute for a person might have multiple values if the person speaks more than one language.
+
+Key Attribute: Key attributes uniquely identify each instance of an entity. They are often used as primary keys in databases. For example, a "Student ID" attribute for a student entity.
+
+Composite Key: A composite key is made up of multiple attributes that together uniquely identify each instance of an entity.
+
+Foreign Key: This attribute is a reference to the primary key of another entity. It establishes a relationship between two entities in a database.
+
+*/
