06166cb5e9b1ad40f83c678ea28111da4afab9de
Md Rijuan <mohammadrijuan2529@gmail.com>
2024-07-29 19:19:57 +0600

    commit

--- Software Development project/Module-7/map_filter_find.js
+++ Software Development project/Module-7/map_filter_find.js
@@ -20,6 +20,48 @@ for (let i=0;i<nums.length;i++)
 }
 console.log(temp);
 
-
+// using map
 const square =nums.map(x=>(x*x))
 console.log(square)
+
+
+
+// filter 
+
+const product=[
+    {name: "samsung",price : 12000, color : "Golden"},
+    {name: "samsung A20",price : 11000, color : "black"},
+    {name: "macbook",price : 125000, color : "silver"},
+    {name: "iphone x",price : 20000, color : "platinum"},
+];
+
+const result = product.filter((pd) => pd.color != "black");
+console.log(result)
+
+
+// find
+const result2 = product.find((pd) => pd.color == "black");
+console.log(result2);
+
+
+// using forEach to see all the object with dynamic value
+const productContainer = document.getElementById("product-container");
+const result3 = product.forEach((product) => {
+    console.log(product);
+    const h1 = document.createElement("h1");
+    h1.innerText = product.name;
+    productContainer.appendChild(h1);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
